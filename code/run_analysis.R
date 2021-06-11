4ade02dbdf726757bf804a98bbd1de400d03422e
rkennedy-rpk <78879824+rkennedy-rpk@users.noreply.github.com>
2021-06-11 16:59:18 -0700

    This is the initial draft at writing the code for the analusis in the final project for Getting and Cleaning Data.  Focus is primarily on writing an efficient data import that minimizes manual entry and removes unnecessary data from the machine once it has been loaded into R.

---
+++ code/run_analysis.R
@@ -0,0 +1,49 @@
+library(here)
+library(tidyverse)
+
+download.file(zipURL, temp)
+filenames <- unzip(temp, list = T)
+filenames <- filter(filenames, Length > 0)
+filenames <- filenames$Name
+
+list_of_file <- filenames %>% 
+        map(function (path){
+                read_table(temp, path)
+        })
+#master <- 
+        list_of_file %>% 
+        map(as_data_frame) %>%  
+        #bind_rows(.id = "id")
+        list2env(test2, envir = .GlobalEnv) #adds contents of list to global environment
+                                            #currently not workings as names(list_of_file) = NULL                
+        
+        
+        
+test <- read_table(temp)
+str(test)
+read_table(temp, "features.txt")
+
+#MANUAL PLAN ----
+zipURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(zipURL,here("data"))
+testPATH <- here("data", "UCI HAR Dataset", "test", "X_test.txt")
+testPATHlabels <- here("data", "UCI HAR Dataset", "test", "y_test.txt")
+trainPATH <- here("data", "UCI HAR Dataset", "train", "X_train.txt")
+trainPATHlabels <- here("data", "UCI HAR Dataset", "train", "y_train.txt")
+featuresPATH <- here("data", "UCI HAR Dataset", "features.txt")
+featuresinfoPATH <- here("data", "UCI HAR Dataset", "features_info.txt")
+testdata <- read.table(testPATH)
+testlabels <- read.table(testPATHlabels)
+traindata <- read.table(trainPATH)
+trainlabels <- read.table(trainPATHlabels)
+features <- read.table(featuresPATH)
+featuresinfo <- read.table(featuresinfoPATH)
+
+mergedata <- bind_rows(traindata, testdata, .id = "setnumber")
+mean <- mergedata %>% 
+        summarise_all(mean) 
+stdv <- mergedata %>% 
+        summarise_all(sd)
+
+
+str(trainlabels)
