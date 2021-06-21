4b7110888ba91a16a261834c7ebf23c4484431a8
rkennedy-rpk <78879824+rkennedy-rpk@users.noreply.github.com>
2021-06-21 14:10:33 -0700

    Updated "run_analysis" as I start removing pieces of my original code that I haven't been able to fix

--- code/run_analysis.R
+++ code/run_analysis.R
@@ -6,23 +6,63 @@ zipURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20
 download.file(zipURL,here("data"))
 testPATHx <- here("data", "UCI HAR Dataset", "test", "X_test.txt")
 testPATHy <- here("data", "UCI HAR Dataset", "test", "y_test.txt")
+testPATHs <- here("data", "UCI HAR Dataset", "test", "subject_test.txt")
 trainPATHx <- here("data", "UCI HAR Dataset", "train", "X_train.txt")
 trainPATHy <- here("data", "UCI HAR Dataset", "train", "y_train.txt")
-features <- here("data", "UCI HAR Dataset", "features.txt")
+trainPATHs <- here("data", "UCI HAR Dataset", "train", "subject_train.txt")
+featuresPATH <- here("data", "UCI HAR Dataset", "features.txt")
+activitylabelsPATH <- here("data", "UCI HAR Dataset", "activity_labels.txt")
 
 
 testx <- read.table(testPATHx)
 testy <- read.table(testPATHy)
+tests <- read.table(testPATHs)
 trainx <- read.table(trainPATHx)
 trainy <- read.table(trainPATHy)
+trains <- read.table(trainPATHs)
 features <- read.table(featuresPATH)
+activitylabels <- read.table(activitylabelsPATH)
+
+#ID the two groups
+tests <- mutate(tests,
+                group = "test")
+trains <- mutate(trains,
+                group = "trains")
+
+# merge the datasets
+Xfull <- bind_rows(testx, trainx)
+Yfull <- bind_rows(testy, trainy)
+Sfull <- bind_rows(tests, trains)
+
+# column names
+
 nameslist <- list()
 features$V2 <- as.character(features$V2)
 nameslist <- features$V2
+colnames(Xfull) <- c(nameslist)
+colnames(Yfull) <- c("activity")
+colnames(Sfull) <- c("subject", "group")
 
-colnames(testdata) <- c(nameslist)
-mergedata <- bind_rows(traindata, testdata, .id = "setnumber")
+#merge into single dataset
+Full <- bind_cols(Xfull, Yfull, Sfull)
+str(FullT)
+FullT <- tibble::as_tibble(Full)
+rm(Full)
 
+#confirm column names are unique
 
-names(testdata)
-head(testdata)
+FullT <- janitor::clean_names(FullT)
+any(duplicated(names(FullT)))
+
+valid_column_names <- make.names(names=names(FullT), unique=TRUE, allow_ = TRUE)
+names(FullT) <- valid_column_names
+
+#extract only "mean" or "standard deviation" variables
+FullTextract <- FullT %>%
+        select(group, subject, activity, contains(".mean."), contains(".std.")) %>% 
+        mutate(activity = factor(activity, labels = activitylabels$V2)) %>% 
+        gather(Measurement, Meas_val, -(group:activity)) %>%
+        separate(Measurement, c("Feature_Variable", "Stat_Type", "Axis"))
+tidy <- FullTextract %>% 
+        group_by(activity, subject) %>% 
+        summarize(Avg_val = mean(Meas_val))
