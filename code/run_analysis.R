a87e8700edddab1b86c584000cc54b666dbe46d1
rkennedy-rpk <78879824+rkennedy-rpk@users.noreply.github.com>
2021-06-21 15:44:55 -0700

    Final big clean on script before cleaning up the comments and submitting.
    I fixed the whole import section so that the data is downloaded to the user's machine in a consistent manner from a URL and all of the file writing is done as efficiently as possible.
    
    In the new line 30 I found an easier way to add names from a vector.  I thought I had to concacenate the vector, but instead I just pass it directly to the colnames function.
    
    I couldn't figure out how to use janitor:clean_names() on the dataset in a way that allowed me to easily separate the variable measurements from the averages of different factors.
    
    In line 49 I've added the necessary code to meet requirement 3 which is "uses descriptive names to name the activities in the data set"
    
    In lines 53 to 56 I address the final prompt 5 "From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject." and finished the summary table by grouping by different variables in appropriate order and then tidy-ing by spreading the "mean" and "std" in "Stat_Type" into separate columns with the newly calculated "Avg_val" as their observations
    
    Finally, I wrote the Summary Table into the folder "outputs" as "summary_dataset.txt"

--- code/run_analysis.R
+++ code/run_analysis.R
@@ -4,24 +4,15 @@ library(here)
 #MANUAL PLAN ----
 zipURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(zipURL,here("data"))
-testPATHx <- here("data", "UCI HAR Dataset", "test", "X_test.txt")
-testPATHy <- here("data", "UCI HAR Dataset", "test", "y_test.txt")
-testPATHs <- here("data", "UCI HAR Dataset", "test", "subject_test.txt")
-trainPATHx <- here("data", "UCI HAR Dataset", "train", "X_train.txt")
-trainPATHy <- here("data", "UCI HAR Dataset", "train", "y_train.txt")
-trainPATHs <- here("data", "UCI HAR Dataset", "train", "subject_train.txt")
-featuresPATH <- here("data", "UCI HAR Dataset", "features.txt")
-activitylabelsPATH <- here("data", "UCI HAR Dataset", "activity_labels.txt")
 
-
-testx <- read.table(testPATHx)
-testy <- read.table(testPATHy)
-tests <- read.table(testPATHs)
-trainx <- read.table(trainPATHx)
-trainy <- read.table(trainPATHy)
-trains <- read.table(trainPATHs)
-features <- read.table(featuresPATH)
-activitylabels <- read.table(activitylabelsPATH)
+testx <- read.table(unz("data/fullsets.zip", "UCI HAR Dataset/test/X_test.txt"))
+testy <- read.table(unz("data/fullsets.zip", "UCI HAR Dataset/test/y_test.txt"))
+tests <- read.table(unz("data/fullsets.zip", "UCI HAR Dataset/test/subject_test.txt"))
+trainx <- read.table(unz("data/fullsets.zip", "UCI HAR Dataset/train/X_train.txt"))
+trainy <- read.table(unz("data/fullsets.zip", "UCI HAR Dataset/train/y_train.txt"))
+trains <- read.table(unz("data/fullsets.zip", "UCI HAR Dataset/train/subject_train.txt"))
+features <- read.table(unz("data/fullsets.zip", "UCI HAR Dataset/features.txt"))
+activitylabels <- read.table(unz("data/fullsets.zip", "UCI HAR Dataset/activity_labels.txt"))
 
 #ID the two groups
 tests <- mutate(tests,
@@ -36,16 +27,12 @@ Sfull <- bind_rows(tests, trains)
 
 # column names
 
-nameslist <- list()
-features$V2 <- as.character(features$V2)
-nameslist <- features$V2
-colnames(Xfull) <- c(nameslist)
+colnames(Xfull) <- features$V2
 colnames(Yfull) <- c("activity")
 colnames(Sfull) <- c("subject", "group")
 
 #merge into single dataset
 Full <- bind_cols(Xfull, Yfull, Sfull)
-
 FullT <- tibble::as_tibble(Full)
 rm(Full)
 
@@ -58,10 +45,18 @@ names(FullT) <- valid_column_names
 #extract only "mean" or "standard deviation" variables
 
 FullTextract <- FullT %>%
-        select(group, subject, activity, contains(".mean."), contains(".std.")) %>% 
-        mutate(activity = factor(activity, labels = activitylabels$V2)) %>% 
+        select(group, subject, activity, contains(".mean."), contains(".std.")) %>% #I added the periods in contains() to eliminate the averages of the signals which are not means of measurements
+        mutate(activity = factor(activity, labels = activitylabels$V2)) %>%
         gather(Measurement, Meas_val, -(group:activity)) %>%
         separate(Measurement, c("Feature_Variable", "Stat_Type", "Axis"))
 tidy <- FullTextract %>% 
-        group_by(activity, subject) %>% 
-        summarize(Avg_val = mean(Meas_val))
+        group_by(activity, subject, Feature_Variable, Stat_Type, Axis) %>% 
+        summarize(Avg_val = mean(Meas_val)) %>% 
+        spread(Stat_Type, Avg_val) %>% 
+        rename("Mean" = mean, "Std" = std, "Activity" = activity, "Subject" = subject)
+tidy %>% 
+write.table("outputs/summary_dataset.txt", row.names = F)
+
+
+
+
